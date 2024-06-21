7eef4e9cbe85c83be96c16331fa9cf1645fd2559
IgorYev <123358377+IgorYev@users.noreply.github.com>
2024-06-21 12:51:37 +0300

    status changes

--- src/server.js
+++ src/server.js
@@ -14,13 +14,13 @@ const setupServer = () => {
   app.get('/contacts', async (req, res) => {
     try {
       const result = await Contact.find();
-      res.json({
-        status: 200,
+      res.status(200).json({
+        status: res.statusCode,
         message: 'Successfully found contacts!',
         data: result,
       });
     } catch (err) {
-      res.status(500).json({ message: 'Server Error' });
+      res.status(500).json({ status: res.statusCode, message: 'Server Error' });
     }
   });
 
@@ -29,21 +29,23 @@ const setupServer = () => {
     try {
       const contact = await Contact.findById(contactId);
       if (contact) {
-        res.json({
-          status: 200,
+        res.status(200).json({
+          status: res.statusCode,
           message: `Successfully found contact with id ${contactId}!`,
           data: contact,
         });
       } else {
-        res.status(404).json({ message: 'Contact not found' });
+        res
+          .status(404)
+          .json({ status: res.statusCode, message: 'Contact not found' });
       }
     } catch (err) {
-      res.status(500).json({ message: 'Server Error' });
+      res.status(500).json({ status: res.statusCode, message: 'Server Error' });
     }
   });
 
   app.use((req, res, next) => {
-    res.status(404).json({ message: 'Not found' });
+    res.status(404).json({ status: res.statusCode, message: 'Not found' });
   });
 
   const PORT = process.env.PORT || 3000;
