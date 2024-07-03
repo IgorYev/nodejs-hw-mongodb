969f98dc6dcc2a48aa22697e5ff3ff72030df430
IgorYev <igorekevdokimovvv@gmail.com>
2024-07-03 12:31:54 +0300

    rework error

--- src/validation/contacts-schemas.js
+++ src/validation/contacts-schemas.js
@@ -2,12 +2,16 @@ import Joi from 'joi';
 
 export const contactSchema = Joi.object({
   name: Joi.string().min(3).max(20).required(),
-  email: Joi.string().email().required(),
-  phone: Joi.string().pattern(/^[0-9]+$/).min(3).max(20).required(),
+  phoneNumber: Joi.string().pattern(/^[0-9]+$/).min(3).max(20).required(),
+  email: Joi.string().email().optional(),
+  isFavourite: Joi.boolean().optional(),
+  contactType: Joi.string().valid('work', 'home', 'personal').optional(),
 });
 
 export const updateContactSchema = Joi.object({
   name: Joi.string().min(3).max(20),
+  phoneNumber: Joi.string().pattern(/^[0-9]+$/).min(3).max(20),
   email: Joi.string().email(),
-  phone: Joi.string().pattern(/^[0-9]+$/).min(3).max(20),
-}).or('name', 'email', 'phone');
+  isFavourite: Joi.boolean(),
+  contactType: Joi.string().valid('work', 'home', 'personal'),
+}).or('name', 'phoneNumber', 'email', 'isFavourite', 'contactType');
